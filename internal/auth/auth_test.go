4367b458d05290ab7f14e7c958d4756729defbe4
AmineSHA <amine.chaouachi@outlook.com>
2024-09-17 22:02:47 +0200

    FInally fixed

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,11 +9,13 @@ import (
 
 func TestGetAPIKeyEmpty(t *testing.T) {
 	testHeader := http.Header{}
-	testHeader.Add("Authorization", " ")
+	testHeader.Add("Authorization", "")
 	got, gotError := GetAPIKey(testHeader)
-	want, wantError := " ", errors.New("no authorization header included")
+	want, wantError := "", errors.New("no authorization header included")
+	gotErrorMsg := gotError.Error()
+	wantErrorMsg := wantError.Error()
 	if !reflect.DeepEqual(want, got) {
-		t.Fatalf("expected: %v - %v, got: %v - %v", want, wantError.Error(), got, gotError.Error)
+		t.Fatalf("expected: %v - %v, got: %v - %v", want, wantErrorMsg, got, gotErrorMsg)
 	}
 
 }
