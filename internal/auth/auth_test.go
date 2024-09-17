a4614f076c49ee028c77c6334aa8bbad5e13ccce
AmineSHA <amine.chaouachi@outlook.com>
2024-09-17 21:50:34 +0200

    Fixed space

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -9,11 +9,11 @@ import (
 
 func TestGetAPIKeyEmpty(t *testing.T) {
 	testHeader := http.Header{}
-	testHeader.Add("Authorization", "")
+	testHeader.Add("Authorization", " ")
 	got, gotError := GetAPIKey(testHeader)
 	want, wantError := " ", errors.New("no authorization header included")
 	if !reflect.DeepEqual(want, got) {
-		t.Fatalf("expected: %v - %v, got: %v - %v", want, wantError, got, gotError)
+		t.Fatalf("expected: %v - %v, got: %v - %v", want, wantError.Error(), got, gotError.Error)
 	}
 
 }
